a1415fc2ae89f88f6337f75adf317ecfcf1520d3
R4MA26 <ramaismaya25@gmail.com>
2022-10-07 14:38:02 +0800

    first commit

---
+++ routes/hello/index.dart
@@ -0,0 +1,9 @@
+import 'package:dart_frog/dart_frog.dart';
+
+Response onRequest(RequestContext context) {
+  print(context.request.method);
+  print(context.request.uri);
+  print(context.request.url);
+
+  return Response(body: 'Welcome Rama to Dart Frog! from route hello');
+}
